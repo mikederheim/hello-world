b555333a789f78a2970367906432b9858c832a70
Mike Derheim <mike.derheim@mivent.com>
2017-01-21 17:54:00 -0700

    Who knows.

--- test.c
+++ test.c
@@ -4,9 +4,4 @@
 int main() {
   printf("hello to all the worlds\n");
   printf("This is a quick change\n");
-<<<<<<< HEAD
-  printf("change11");
-=======
-  printf("blurg\n");
->>>>>>> 65d5e4e9d4cd954911f6a451484a32de39fba5aa
 }
